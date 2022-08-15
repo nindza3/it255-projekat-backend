de8ad01ad32ab8a8e3bf54ebf5a10d955eb84553
masa.petrovic <petrovicmasa@outlook.com>
2022-08-15 22:01:43 +0200

    add main ts updates

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { MysqlDataSource } from 'typeOrm.config';
+import { MovieModule } from './movies/movies.module';
+import { RoleModule } from './roles/role.module';
+import { UserModule } from './users/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(MysqlDataSource.options)],
+  imports: [
+    TypeOrmModule.forRoot(MysqlDataSource.options),
+    RoleModule,
+    UserModule,
+    MovieModule,
+  ],
   controllers: [],
   providers: [],
 })
